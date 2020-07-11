2d27fac8fbe6a2781b665af051b8815abdad0726
liangkuncao <liangkuncao@gmail.com>
2020-07-12 01:25:45 +0800

    July 11

--- leetcode-journey/0102_binary_tree_level_order_traversal_medium.py
+++ leetcode-journey/0102_binary_tree_level_order_traversal_medium.py
@@ -22,3 +22,39 @@ class Solution:
             res.append(curr_vals)
             curr_level, next_level = next_level, []
         return res
+
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+
+        levels = [[root]]
+        for level in levels:
+            nodes = []
+            for node in level:
+                if node.left:
+                    nodes.append(node.left)
+                if node.right:
+                    nodes.append(node.right)
+            if nodes:
+                levels.append(nodes)
+        return [[node.val for node in level] for level in levels]
+
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        res = []
+
+        def get_level(root: TreeNode, res, depth):
+            if not root:
+                return None
+
+            if len(res) == depth:
+                res.append([])
+            res[depth].append(root.val)
+            get_level(root.left, res, depth + 1)
+            get_level(root.right, res, depth + 1)
+
+        get_level(root, res, 0)
+        return res
