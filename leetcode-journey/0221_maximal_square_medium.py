883467ba3afd2f4aae464a390d457b8b2a09a143
liangkuncao <liangkuncao@gmail.com>
2020-08-03 00:51:12 +0800

    Aug 2

--- leetcode-journey/0221_maximal_square_medium.py
+++ leetcode-journey/0221_maximal_square_medium.py
@@ -1,17 +1,23 @@
+from typing import List
+
+
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         if not matrix:
             return 0
 
-        row_len, col_len = len(matrix), len(matrix[0])
-        max_breadth = 0
-        dp = [[0 for _ in range(col_len)] for _ in range(row_len)]
-        for row in range(row_len):
-            for col in range(col_len):
-                if matrix[row][col] == '1':
-                    if row == 0 or col == 0:
-                        dp[row][col] = 1
+        max_side = 0
+        rows, cols = len(matrix), len(matrix[0])
+        dp = [[0 for _ in range(cols)] for _ in range(rows)]
+        for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j] == '1':
+                    if i == 0 or j == 0:
+                        dp[i][j] == 1
                     else:
-                        dp[row][col] = min(dp[row - 1][col - 1], dp[row - 1][col], dp[row][col - 1]) + 1
-                    max_breadth = max(max_breadth, dp[row][col])
-        return max_breadth ** 2
+                        dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
+                    max_side = max(max_side, dp[i][j])
+        return max_side ** 2
+
+
+print(Solution().maximalSquare([['1']]))
