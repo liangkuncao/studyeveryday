37ccf364a1fda46b9dd01be9009dbddbf5284539
liangkuncao <liangkuncao@gmail.com>
2020-09-30 20:03:59 +0800

    leetcode hard

--- leetcode-journey/0139_word_break_medium.py
+++ leetcode-journey/0139_word_break_medium.py
@@ -1,30 +1,10 @@
-from typing import List
-
 class Solution:
-    """out of time limit"""
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        if not s:
-            return True
-
-        res = False
-        for i in range(len(s)):
-            if s[:i + 1] in wordDict:
-                res = True and self.wordBreak(s[i + 1:], wordDict)
-            if res:
-                return res
-        return res
-
-class Solution2:
-    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        n = len(s)
-        can_break = [1] + [0] * n
-        for loc in range(n):
-            if not can_break[loc]:
-                continue
+        dp = [True] + [False] * len(s)
+        for i in range(1, len(s) + 1):
             for word in wordDict:
-                if s[loc: loc + len(word)] == word:
-                    can_break[loc + len(word)] = 1
-        return can_break[-1]
-
-
-print(Solution().wordBreak('leetcode', ["leet", "code"]))
+                prev, curr = i - len(word), i
+                if prev >= 0 and dp[prev] and s[prev:curr] == word:
+                    dp[curr] = True
+                    break
+        return dp[-1]
